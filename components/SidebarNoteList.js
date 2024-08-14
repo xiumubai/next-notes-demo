3a7ad9f331a00741a544ebb420c2ef11f337c01a
guojianli <1547702880@qq.com>
2024-08-14 10:52:41 +0800

    feat: React Notes Demo 的效果全部实现

---
+++ components/SidebarNoteList.js
@@ -0,0 +1,29 @@
+import SidebarNoteListFilter from '@/components/SidebarNoteListFilter';
+import SidebarNoteItemHeader from '@/components/SidebarNoteItemHeader';
+import { getAllNotes } from '@/lib/redis';
+import { sleep } from '@/lib/utils';
+
+export default async function NoteList() {
+
+  await sleep(2000)
+  const notes = await getAllNotes()
+
+  if (Object.entries(notes).length == 0) {
+    return <div className="notes-empty">
+      {'No notes created yet!'}
+    </div>
+  }
+
+  return (
+    <SidebarNoteListFilter notes = {
+      Object.entries(notes).map(([noteId, note]) => {
+        const noteData = JSON.parse(note)
+        return {
+          noteId,
+          note: noteData,
+          header: <SidebarNoteItemHeader title={noteData.title} updateTime={noteData.updateTime} />
+        }
+      })
+    } />
+  )
+}
